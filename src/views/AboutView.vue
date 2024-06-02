9ba0abb9d2ae4ebac46a97c07f0db2a0701f6263
xkylogul <30959439+xkylogul@users.noreply.github.com>
2024-06-02 11:50:01 +0800

    main_bata-1

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>about</h1>
+    <el-button 
+    type="primary">Primary
+    </el-button>
+  </div>
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 60vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
